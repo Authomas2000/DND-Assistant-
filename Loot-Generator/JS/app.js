ff3bec306e8bc5ffed2cbb85624c9a0e7a5a9f9c
Authomas2000 <austintho@yahoo.com>
2022-06-25 23:19:00 -0400

    Dropdown Bar and Style Update

--- Loot-Generator/JS/app.js
+++ Loot-Generator/JS/app.js
@@ -1,15 +1,39 @@
+//Dropdown animation setup
 document.addEventListener("click", e => {
-    const isDropdownButton = e.target.matches("[data-dropdown-button]")
-    if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
-  
-    let currentDropdown
-    if (isDropdownButton) {
-      currentDropdown = e.target.closest("[data-dropdown]")
-      currentDropdown.classList.toggle("active")
-    }
-  
-    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-      if (dropdown === currentDropdown) return
-      dropdown.classList.remove("active")
-    })
+  const isDropdownButton = e.target.matches("[data-dropdown-button]")
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
+
+  let currentDropdown
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]")
+    currentDropdown.classList.toggle("active")
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+    if (dropdown === currentDropdown) return
+    dropdown.classList.remove("active")
   })
+})
+
+//Dropdown variable setup
+biome_dropdown.addEventListener("biome", setBiome);
+creature_dropdown.addEventListener("creature", setCreature);
+amount_dropdown.addEventListener("amount", setAmount);
+
+//Function to setup biome variables based off of dropdown menu
+function setBiome(event) {
+  biome_dropdown.innerHTML="Biome [" + event.target.id + "]";
+  biome = event.target.id;
+}
+
+//Function to setup creature variables based off of dropdown menu
+function setCreature(event) {
+  creature_dropdown.innerHTML="Creature [" + event.target.id + "]";
+  creature = event.target.id;
+}
+
+//Function to setup amount of loot variables based off of dropdown menu
+function setAmount(event) {
+  amount_dropdown.innerHTML="Amount [" + event.target.id + "]";
+  amount = event.target.id;
+}
--- Loot-Generator/JS/variables.js
+++ Loot-Generator/JS/variables.js
@@ -1,1 +1,11 @@
+//Biome variables
+let biome_dropdown = document.getElementById("biome-dropdown");
+let biome = "";
+
+//creature variables
+let creature_dropdown = document.getElementById("creature-dropdown");
+let creature = "";
+
+//creature variables
+let amount_dropdown = document.getElementById("amount-dropdown");
 let amount = 0;
